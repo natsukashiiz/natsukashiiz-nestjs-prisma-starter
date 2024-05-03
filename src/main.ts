0d59e8b69f17f4be0e99de3e3c8f21877c2c4f6b
ɴᴀᴛsᴜᴋᴀsʜɪɪᴢ <74323401+natsukashiiz@users.noreply.github.com>
2024-05-03 11:03:30 +0700

    update: swagger

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,17 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const swaggerPath = process.env.SWAGGER_PATH || 'document';
   const config = new DocumentBuilder()
     .setTitle('NestJS 101 API')
     .setDescription('NestJS 101 API description')
     .setVersion('1.0')
     .addTag('NestJS 101')
+    .setExternalDoc('JSON', `${swaggerPath}-json`)
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('document', app, document, {
+  SwaggerModule.setup(swaggerPath, app, document, {
     swaggerOptions: {
       persistAuthorization: true,
     },
